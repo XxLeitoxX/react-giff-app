29a1680c506d13bf4fbaf5f7f7264149ea6c9b09
Leonardo <leonardojgimenez@gmail.com>
2020-10-19 12:28:16 -0300

    docs agregado

---
+++ docs/precache-manifest.72d8846b4eac1c29bf15cc49c7e8ebc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de41240ad6bab1b284308b07209936a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b0249ff3f200c43034f",
+    "url": "/static/css/main.ac87b5c9.chunk.css"
+  },
+  {
+    "revision": "c891655c70870ca4f1e5",
+    "url": "/static/js/2.00eec8fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00eec8fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b0249ff3f200c43034f",
+    "url": "/static/js/main.3475c5b5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
